1a849e2679dfd1ff63ca976a99e1d694c2afcc3b
Hariom Singh <suppraaaa2003@gmail.com>
2026-01-20 01:27:12 +0530

    you

--- ThreeJsM/src/main.js
+++ ThreeJsM/src/main.js
@@ -1,4 +1,5 @@
 import * as THREE from 'three';
+import { OrbitControls} from "three/addons/controls/OrbitControls.js";
 
 const canvas = document.getElementById('canvas');
 if (!(canvas instanceof HTMLCanvasElement)) {
@@ -40,4 +41,42 @@ const renderer = new THREE.WebGLRenderer({canvas});
 
 renderer.setSize(window.innerWidth, window.innerHeight);
 
+renderer.setPixelRatio(window.devicePixelRatio);
+
+
+// 6. Add OrbitControls
+const controls = new OrbitControls(camera, renderer.domElement);
+controls.enableDamping = true;
+controls.dampingFactor = 0.05;
+controls.enableZoom = true;
+controls.enablePan = true;
+
+// 7. add animations
+
+function animate() {
+    requestAnimationFrame(animate);
+
+    dodecahedron.rotation.x += 0.01;
+    dodecahedron.rotation.y += 0.01;
+
+
+    box.rotation.y += 0.005;
+
+    controls.update();
+
 renderer.render(scene, camera);
+};
+
+
+// 8, handle window. resizing
+window.addEventListener('resize', ()=>{
+    camera.aspect =  window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+});
+
+
+
+animate();
+
+
